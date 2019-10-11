6344976405aaa08fb1e7a39450daabc3cd664b09
krijovnichek <tyukaevruslan@gmail.com>
2019-10-11 14:09:27 +0400

    Add Test Files

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print ("test")
+import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+os.system('cls') # Clear Terminal
+
+
+path = r"D:\Projects\Ruslan\dev\testPath\pycourse" # Test files folder
+filename = "file1.jpg"
+tfile = os.path.join(path, filename)
+
+
+print (list(TAGS.values()))   
+
+
+
+
+##  Walk through path
+# for root, dirs, files in os.walk(path):
+#     print(root)
+#     for _dir in dirs:
+#         print(_dir)
+ 
+#     for _file in files:
+#         print(_file)
+        
